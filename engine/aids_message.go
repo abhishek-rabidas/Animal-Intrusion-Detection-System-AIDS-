547a36c54d68483e9ab41e4a05fd20adf255d208
abhishek <avisec07@gmail.com>
2023-06-02 00:21:53 +0530

    trigger api

---
+++ engine/aids_message.go
@@ -0,0 +1,6 @@
+package engine
+
+type DetectionMessage struct {
+	Timestamp string
+	Class     string
+}
--- engine/element_trigger_preprocess.go
+++ engine/element_trigger_preprocess.go
@@ -1,18 +1,32 @@
 package engine
 
 import (
+	"bytes"
+	"encoding/json"
 	log "github.com/sirupsen/logrus"
+	"net/http"
+	"time"
 )
 
-func Trigger() {
-	log.Info("Trigger Called")
+func Trigger(class string) {
+	msg := DetectionMessage{
+		Timestamp: time.Now().Format("02-01-2006 15:04:05"),
+		Class:     class,
+	}
+
+	payload, _ := json.Marshal(msg)
+
+	post, err := http.Post("http://localhost:55555", "application/json", bytes.NewReader(payload))
+	if err != nil || post.StatusCode != 200 {
+		log.Error(err)
+	}
 }
 
 func CheckThreshold(confidences []float32, triggerThreshold float32, classes []string, classIds []int) {
 	for _, classId := range classIds {
 		//TODO:Change to animal/tiger/leopard
 		if classes[classId] == "truck" && confidences[classId] >= triggerThreshold {
-			Trigger()
+			Trigger("truck")
 			break
 		}
 	}
